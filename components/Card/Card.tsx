65257e9d71347a63c8f2df751da70eca0aaf8d97
neilZon <neilcviloria@gmail.com>
2022-01-09 12:24:59 -0700

    initial components

---
+++ components/Card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = ({ img, title, date, description }) => {
+  return (
+    <div className="bg-white rounded-xl">
+      <div>img</div>
+      <div>descriptions</div>
+    </div>
+  );
+};
+
+export default Card;
